1db8186a390acb4bac51d620b8b3914c3f2093ed
Bismarck <bisanbl94@gmail.com>
2019-09-29 19:19:30 -0600

    Nuevas caracteristicas Agregadas
    Capacidad para saber si los paquetes se instalaron correctamente.
    Logfile agregado para futuras revisiones. 
    
    Resuelto un error que no agregaba el usuario del sistema a los grupos. y en su lugar colocaba al usuario root.

--- gns3.sh
+++ gns3.sh
@@ -1,29 +1,58 @@
 #!/usr/bin/env bash
-if [ $USER = root ]; then
-  add-apt-repository ppa:gns3/ppa
+function chekinstall() {
+#Comprobamos si esta instalados los paquetes mediante el comando dpkg
+  for i in gns3-gui gns3-server gns3-iou dynamips:i386 docker.io
+  do
+    aux=$(dpkg -s $i | grep "Status: *")
+    if [[ $aux != "Status: install ok installed" ]]
+    then
+      echo "el paquete $i fallo en su instalacion" >> /var/log/gns3installlog
+      return 0
+    fi
+  done
+  return 1
+}
+echo "GNS3 Intall Log. Fecha: $(date +%d-%m-%Y) $(date +%H:%M) " > /var/log/gns3installlog
+if [ "$EUID" -eq 0 ]; then
+  add-apt-repository -y ppa:gns3/ppa >> /var/log/gns3installlog
   dpkg --add-architecture i386
-  sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+  echo "Preparando todo."
+  sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common >> /var/log/gns3installlog
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(. /etc/os-release; echo "$UBUNTU_CODENAME") stable"
-  sudo apt update
-  sudo apt remove -y docker docker-engine docker.io
-  sudo apt install -y gns3-gui gns3-server gns3-iou dynamips:i386  docker.io
+  echo "Actualizando tu sistema."
+  sudo apt update >> /var/log/gns3installlog
+  echo "Eliminando conflictos."
+  sudo apt remove -y docker docker-engine docker.io >> /var/log/gns3installlog
+  echo "Instalando el entorno"
+  sudo apt install -y gns3-gui gns3-server gns3-iou dynamips:i386  docker.io >> /var/log/gns3installlog
 
-  sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-  sudo chmod +x /usr/local/bin/docker-compose
+  sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose >> /var/log/gns3installlog
+  sudo chmod +x /usr/local/bin/docker-compose >> /var/log/gns3installlog
 
-  sudo groupadd docker
+  echo "Configurando tu usuario."
+  sudo groupadd docker >> /var/log/gns3installlog
 
-  sudo usermod -aG docker ${USER}
-  sudo usermod -aG bridge ${USER}
-  sudo usermod -aG libvirt ${USER}
-  sudo usermod -aG kvm ${USER}
-  sudo usermod -aG wireshark ${USER}
+  user=$(w|awk 'NR>2 {print $1}')
 
-  docker pull bisanbl/frrouting
-  docker pull bisanbl/debian-host
+  sudo usermod -aG docker $user >> /var/log/gns3installlog
+  sudo usermod -aG bridge $user >> /var/log/gns3installlog
+  sudo usermod -aG libvirt $user >> /var/log/gns3installlog
+  sudo usermod -aG kvm $user >> /var/log/gns3installlog
+  sudo usermod -aG wireshark $user >> /var/log/gns3installlog
 
-  reboot
+  echo "Descargando Contenedores."
+  docker pull bisanbl/frrouting >> /var/log/gns3installlog
+  docker pull bisanbl/debian-host >> /var/log/gns3installlog
+  echo "Estara todo bien? "
+  chekinstall
+  if [ $? = "1"]; then
+    read -n 1 -s -r -p "Instalacion Exitosa!! Presiona una tecla para cerrar sesion. "
+    echo "Instalacion Exitosa!!!. Cerrando Sesion..." >> /var/log/gns3installlog
+    killall5
+  else
+    echo "Algo salio mal :C | la instalacion no fue Exitosa." >> /var/log/gns3installlog
+  fi
 else
-  echo "ERROR!!! Ejecute el script como super usuario..."
+  echo "ERROR!!! Ejecute el script como super usuario..." >> /var/log/gns3installlog
 fi
