7d71abd96f662ca08d4cfbb6505532b03d69761d
Bismarck <bisanbl94@gmail.com>
2019-09-30 07:26:27 -0600

    Logica Corregida
    Errores de logica corregida. resuelto un problema que cerraba sesion en root en lugar del usuario de escritorio.

--- gns3.sh
+++ gns3.sh
@@ -26,34 +26,37 @@ if [ "$EUID" -eq 0 ]; then
   echo "Paso 3/6 : Eliminando conflictos."
   sudo apt-get remove -y docker docker-engine docker.io >> /var/log/gns3installlog
   echo "Paso 4/6 : Instalando el entorno."
-  sudo apt-get install -y gns3-gui gns3-server gns3-iou dynamips:i386  docker.io >> /var/log/gns3installlog
+  sudo apt-get -y install wireshark
+  sudo apt-get install -y gns3-gui gns3-server gns3-iou dynamips:i386  docker.io
 
   sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose >> /var/log/gns3installlog
   sudo chmod +x /usr/local/bin/docker-compose >> /var/log/gns3installlog
-
-  echo "Paso 5/6 : Configurando."
-  sudo groupadd docker >> /var/log/gns3installlog
-
-  user=$(w|awk 'NR>2 {print $1}')
-
-  sudo usermod -aG docker $user >> /var/log/gns3installlog
-  sudo usermod -aG bridge $user >> /var/log/gns3installlog
-  sudo usermod -aG libvirt $user >> /var/log/gns3installlog
-  sudo usermod -aG kvm $user >> /var/log/gns3installlog
-  sudo usermod -aG wireshark $user >> /var/log/gns3installlog
-
-  echo "Paso 6/6 : Descargando Contenedores."
-  docker pull bisanbl/frrouting >> /var/log/gns3installlog
-  docker pull bisanbl/debian-host >> /var/log/gns3installlog
-  echo "Estara todo bien? "
   chekinstall
   if [ $? = "1" ]; then
+    echo "Paso 5/6 : Configurando."
+    sudo groupadd docker >> /var/log/gns3installlog
+
+    user=$(w|awk 'NR>2 {print $1}')
+
+    sudo usermod -aG docker $user >> /var/log/gns3installlog
+    sudo usermod -aG bridge $user >> /var/log/gns3installlog
+    sudo usermod -aG libvirt $user >> /var/log/gns3installlog
+    sudo usermod -aG kvm $user >> /var/log/gns3installlog
+    sudo usermod -aG wireshark $user >> /var/log/gns3installlog
+
+    echo "Paso 6/6 : Descargando Contenedores."
+    docker pull bisanbl/frrouting >> /var/log/gns3installlog
+    docker pull bisanbl/debian-host >> /var/log/gns3installlog
+    echo "Estara todo bien? "
+
     read -n 1 -s -r -p "Instalacion Exitosa!! Presiona una tecla para cerrar sesion. "
     echo "Instalacion Exitosa!!!. Cerrando Sesion..." >> /var/log/gns3installlog
+    su - $user
     killall5
   else
     echo "Algo salio mal :C | la instalacion no fue Exitosa." >> /var/log/gns3installlog
   fi
+
 else
   echo "ERROR!!! Ejecute el script como super usuario..." >> /var/log/gns3installlog
 fi
